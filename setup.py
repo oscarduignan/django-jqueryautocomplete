dbe97646bed84aa7c684a9846fb44de64e5e397f
Oscar Duignan <oscarduignan@gmail.com>
2010-03-09 15:32:02 +0000

    spruced things up a bit for my push to github (license.txt and setup.py)

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='django-jqueryautocomplete',
+    version='0.1',
+    description='Very basic and un-tested proof of concept jQuery auto-complete app based off of Dave Corey\'s jQuery plugin.',
+    author='Oscar Duignan',
+    author_email='oscarduignan@gmail.com',
+    url='http://github.com/oscarduignan/django-jqueryautocomplete/',
+    py_modules=['jqueryautocomplete'],
+    license='BSD'
+)
+
