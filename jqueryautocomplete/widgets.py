deb6c0cbe70b6be7deeb5afa24c779e9e933014d
Oscar Duignan <oscarduignan@gmail.com>
2010-03-09 14:45:33 +0000

    removed models.py, I'll leave that as an exercise for later

--- jqueryautocomplete/widgets.py
+++ jqueryautocomplete/widgets.py
@@ -1,49 +1,57 @@
 import datetime
-from django.forms import widgets
+from django import forms
 from django.utils import simplejson
 from django.utils.safestring import mark_safe
 
 
-def JQueryAutoCompleteInput(widgets.HiddenInput):
-    defaults = u'''{
+class JQueryAutoCompleteTextInput(forms.TextInput):
+    """
+    A text input with jQuery auto completion, using the auto-complete
+    plugin from Dave Corey.
+    
+    (http://www.codenothing.com/demos/2009/auto-complete/docs.html)
+
+    """
+    jquery_defaults = u'''{
+        striped: "auto-complete-striped",
         autofill: true,
-        delay: 200,
-        striped: "alternate"
+        delay: 400
     }'''
 
-    def __init__(self, lookup_url, overrides=None, attrs=None):
-        self.lookup_url = lookup_url
-        self.overrides = overrides
-        super(JQueryAutoCompleteInput, self).__init__(attrs)
-    
-    def render(self, name, value, attrs=None):
-        output = super(JQueryAutoCompleteInput, self).render(name, value, attrs)
-        
-        return output + mark_safe(u'''
-        <input type="text" name="lookup_%(name)s" id="#lookup_%(name)s" class="jquery_lookup" />
+    def __init__(self, ajax=None, data=None, jquery_options=None, attrs=None):
+        """
+        `ajax` is a remote url to retrieve completion possibilities from.
+        `data` is a list of completion possibilities.
+        `jquery_options` is a string representation of a js object containing 
+                         options for the auto-complete plugin.
+        """
+        self.ajax = ajax
+        self.data = data
+        self.jquery_options = jquery_options
+        super(JQueryAutoCompleteTextInput, self).__init__(attrs)
 
+    def render(self, name, value, attrs=None):
+        output = super(JQueryAutoCompleteTextInput, self).render(name, value, attrs)
+
+        source = ''
+        if self.ajax:
+            source = '{ajax: "%s"},' % self.ajax
+        elif self.data:
+            source = '{dataSupply: %s},' % simplejson.dumps(self.data) 
+
+        return output + mark_safe(u'''
         <script type="text/javascript">
             jQuery(document).ready(function (){
-                jQuery("#lookup_%(name)s").autoComplete(JQuery.extend(
-                    {
-                        onSelect: function(event, ui) {
-                             jQuery("#id_%(name)s").val(ui.data.value);
-                        }
-                    },
+                jQuery("#id_%(name)s").autoComplete(jQuery.extend(
                     %(defaults)s,
-                    %(overrides)s
+                    %(source)s
+                    %(options)s
                 ));
             });
         </script>
-        ''')
-    
-    class Media:
-        css = {
-            'all': ('jquery.autocomplete.css',)
-        }
-        js = (
-            'jquery.js',
-            'jquery.autocomplete.js'
-        )
-
-
+        ''' % {
+            'name': name,
+            'defaults': self.jquery_defaults or '{}',
+            'options': self.jquery_options or '{}',
+            'source': source, 
+        })
