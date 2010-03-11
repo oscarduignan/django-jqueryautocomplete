5fa4aabcd59d718059d8d013c3cc1cf40143f4ff
Oscar Duignan <oscarduignan@gmail.com>
2010-03-11 14:46:55 +0000

    fixed an issue with setup.py, module was not being included in dist tarball

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ from distutils.core import setup
 
 setup(
     name='django-jqueryautocomplete',
-    version='0.1',
-    description='Very basic and un-tested proof of concept jQuery auto-complete app based off of Dave Corey\'s jQuery plugin.',
+    version='0.3',
+    description='Very basic and un-tested proof of concept jQuery auto-complete app based off of Corey Hart\'s jQuery plugin.',
     author='Oscar Duignan',
     author_email='oscarduignan@gmail.com',
     url='http://github.com/oscarduignan/django-jqueryautocomplete/',
-    py_modules=['jqueryautocomplete'],
+    packages=['jqueryautocomplete'],
     license='BSD'
 )
 
